47cdfd4c2b3429aee079a47ff0ae24d1ec5034c9
HP <arunagiri9062@mountzion.ac.in>
2024-10-10 10:37:13 +0530

    2

--- src/Pages/Homepage/Home.jsx
+++ src/Pages/Homepage/Home.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import img2 from "../../assets/img2.svg";
+import './Home.css';
 
 const Home = () => {
-  return (
-    <div className="home">
-      <Navbar />
+    return (
+        <div className="home">
+            <Navbar />
 
-      <div className="main">
+            <div className="main">
 
-        <h1>Connectify</h1>
-        <h2>Connectify</h2>
+                <h1>Connectify</h1>
+                <h2>Connectify</h2>
 
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 };
 
 export default Home;
