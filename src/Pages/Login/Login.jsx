22dafcbb68ed46767772c982863e3fce8941a0c2
HP <arunagiri9062@mountzion.ac.in>
2024-10-10 13:36:26 +0530

    login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,6 +4,10 @@ import img2 from "../../assets/img2.svg";
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from '../../Components/Footer/Footer';
 
+const clear = () => {
+  document.getElementById("user").value = "";
+  document.getElementById("pass").value = "";
+}
 const Login = () => {
   return (
     <div className="login">
@@ -11,16 +15,17 @@ const Login = () => {
 
       <div className="main">
         <div className="right-login">
-          <h1>Login</h1>
+          <h1>LOGIN</h1>
           <div className="inputs">
-          <input type="text" placeholder="Username" />
-          <input type="text" placeholder="Password" />
+            <input type="text" id='user' placeholder="Username" />
+            <input type="password" id='pass' placeholder="Password" />
           </div>
 
           <div className="buttons">
-            <button className="clear-btn">Clear</button>
+            <button type='button' onClick={clear} className="clear-btn">Clear</button>
             <button className="submit-btn">Login</button>
           </div>
+          <p>Don't have an account?<a href="#"> Signup</a></p>
         </div>
         <div className="left-login">
           <img src={img2} alt="" />
