0aea1e4702b07478f66eeddb5f50d0d2153489e2
jaasi <jaasirahamed87@gmail.com>
2024-10-10 11:46:39 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Home from './Pages/Homepage/Home'
+import Login from './Pages/Login/Login'
 
 const App = () => {
   return (
     <div>
-      <Home/>
+      {/* <Home/> */}/
+      <Login/>
     </div>
   )
 }
---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Login.css";
+import img2 from "../../assets/img2.svg";
+import Navbar from "../../Components/Navbar/Navbar";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <Navbar />
+
+      <div className="main">
+        <div className="right-login">
+          <h1>Login</h1>
+          <div className="inputs">
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Password" />
+          </div>
+
+          <div className="buttons">
+            <button className="clear-btn">Clear</button>
+            <button className="submit-btn">Login</button>
+          </div>
+        </div>
+        <div className="left-login">
+          <img src={img2} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
