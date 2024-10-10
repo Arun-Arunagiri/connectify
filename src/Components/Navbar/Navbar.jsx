500235be23dfdf28b85228e89bcfd56281182e03
HP <arunagiri9062@mountzion.ac.in>
2024-10-10 10:06:42 +0530

    assets

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Navbar.css'
+import votingmachine from '../../assets/voting Machine 1.png'
+
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='navbar'>
+            <div className="navbar-header">
+                <img src={votingmachine} alt="" />
+                <h2>SMART EVM</h2>
+            </div>
+            <div className="navbar-right">
+                <ul className='navbar-menu'>
+                    <li>Home</li>
+                    <li>Features</li>
+                    <li>Product</li>
+                    <li>Community</li>
+                </ul>
+                <div className="navbar-button">
+                    <button>Register Now</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
