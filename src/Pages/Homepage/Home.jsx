6b1a0f512df5d1b5e275a36ed417934c782bb6c8
jaasi <jaasirahamed87@gmail.com>
2024-10-10 11:01:37 +0530

    summa

--- src/Pages/Homepage/Home.jsx
+++ src/Pages/Homepage/Home.jsx
@@ -1,27 +1,23 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import img2 from "../../assets/img2.svg";
-import './Home.css';
-
-
+import "./Home.css";
+import Footer from "../../Components/Footer/Footer";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <Navbar />
+  return (
+    <div className="home">
+      <Navbar />
 
-            <div className="main">
-
-                <div className="right">
-
-                </div>
-                <div className="left">
-                    <img src={img2} alt="" />
-                </div>
-
-            </div>
+      <div className="main">
+        <div className="right"></div>
+        <div className="left">
+          <img src={img2} alt="" />
         </div>
-    );
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
